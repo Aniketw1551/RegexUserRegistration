df122f5def5da877df90853da72229d2da961c61
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-02-01 21:47:52 +0530

    [Aniket] Adding Validation of First name program

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistration
+{
+    public class Program
+    {
+        /// <summary>
+        /// Validating first name starting with cap and have atlest 3 characters 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Welcome to User Registration Program");
+                UserRegistration1();
+                Console.ReadLine();
+            }
+        }
+            //Method to perform UserRegistration
+            public static void UserRegistration1()
+            {
+                Console.WriteLine("Enter First Name");
+                string firstName = Console.ReadLine();
+                Console.WriteLine(UserRegistration.ValidateFirstName(firstName));
+            }
+    }
+}
