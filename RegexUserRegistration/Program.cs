4c244a5ef90f7811c65dc61edae45bb0f50acf23
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-02-03 21:28:49 +0530

    [Aniket] update program

--- RegexUserRegistration/Program.cs
+++ RegexUserRegistration/Program.cs
@@ -9,40 +9,17 @@ namespace RegexUserRegistration
     public class Program
     {
         /// <summary>
-        /// Validating first name starting with cap and have atlest 3 characters 
+        /// Validating first name starting with cap letter and have atlest 3 characters 
         /// </summary>
-        /// <param name="args"></param>
+        /// <param name="args">The args.</param>
         static void Main(string[] args)
         {
             {
                 Console.WriteLine("Welcome to User Registration Program\n");
-                UserRegisrtation1();
-               UserRegistraton userRegisrtation = new UserRegistraton(); // Declaring object
-            }
-        }
-        //Method to perform UserRegistration
-        public static void UserRegistration1()
-        {
-            bool Continue = true;
-            while (Continue)
-            {
-                Console.WriteLine("Choose Option");
-                Console.WriteLine("1.First Name 0.Exit\n");
-                int choice = int.Parse(Console.ReadLine());
-                switch (choice)
-                {
-                    case 0:
-                        Continue = false;
-                        break;
-                    case 1:
-                       Console.WriteLine("Enter valid first name");
-                        string frstname = Console.ReadLine();
-                        userRegistraton.ValidateFName(frstname);
-                        break;
-                    default:
-                        Console.WriteLine("Please check your option");
-                        break;
-                }
+                UserRegistration userRegistration = new UserRegistration(); // Declaring object
+                Console.WriteLine("Enter valid first name");
+                string firstname = Console.ReadLine();
+                userRegistration.ValidateFName(firstname);
             }
         }
     }
